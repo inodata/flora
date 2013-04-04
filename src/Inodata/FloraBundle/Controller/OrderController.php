60fdca3a7c02f038bfc7975e5bd4f34a39f89a51
Heriberto Monterrubio <heriberto@inodata.com.mx>
2013-04-04 00:39:10 -0600

    Se agrego ajax al agregar productos en la orden

--- src/Inodata/FloraBundle/Controller/OrderController.php
+++ src/Inodata/FloraBundle/Controller/OrderController.php
@@ -4,12 +4,18 @@ namespace Inodata\FloraBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Inodata\FloraBundle\Entity\Product;
 
 class OrderController extends Controller
 {	
-	public function productAction()
+	public function productAction($id)
 	{
+		$product = $this->getDoctrine()
+			->getRepository('InodataFloraBundle:Product')
+			->find($id);
 		
-		return new Response("");
+		$content = $this->renderView('InodataFloraBundle:Order:_product_item.html.twig', array('product' => $product));
+		
+		return new Response($content);
 	}
 }
