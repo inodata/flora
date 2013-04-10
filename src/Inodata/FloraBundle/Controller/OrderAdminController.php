aa9d39af56c7bd8d498474c28a583d84172d2deb
Heriberto Monterrubio <heriberto@inodata.com.mx>
2013-04-10 01:50:11 -0500

    Solucion del conflicto al actualizar orden

--- src/Inodata/FloraBundle/Controller/OrderAdminController.php
+++ src/Inodata/FloraBundle/Controller/OrderAdminController.php
@@ -2,16 +2,21 @@
 
 namespace Inodata\FloraBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Inodata\FloraBundle\Entity\Product;
 use Inodata\FloraBundle\Entity\Order;
 use Inodata\FloraBundle\Entity\OrderProduct;
-//use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
 
 class OrderAdminController extends Controller
 {	
+	/**
+	 * @param mixed $id
+	 *
+	 * @return Response
+	 */
 	public function productAction($id)
 	{
 		$product = $this->getDoctrine()
@@ -27,13 +32,10 @@ class OrderAdminController extends Controller
 		return new Response(json_encode($response));
 	}
 	
-	/**
-	 * 
-	 * @param int $id
-	 * @return json
-	 */
 	public function orderProductsAction($id = null)
 	{
+		$price_subtotal = 0;
+		
 		if($id){
 			$order = $this->getDoctrine()
 				->getRepository('InodataFloraBundle:OrderProduct')
@@ -54,14 +56,77 @@ class OrderAdminController extends Controller
 				->getRepository('InodataFloraBundle:Product')
 				->find($productId);
 				
+				$price_subtotal += $product->getPrice()*$cant;
+				
 				$listFields.= $this->renderView('InodataFloraBundle:Order:_product_item.html.twig', 
 						array('product' => $product, 'total'=>$cant));
 			}
 		}
 		
 		$selectOptions = $this->renderView('InodataFloraBundle:Order:_select_order_option.html.twig', array('products' => $order));
-		$response = array("listFields"=>$listFields, "selectOptions"=>$selectOptions);
+		
+		$response = array("listFields"=>$listFields, "selectOptions"=>$selectOptions, 
+						  'totals'=>$this->getTotalsCost($id, $price_subtotal));
 		
 		return new Response(json_encode($response));
+	}
+	/**
+	 * Calculate total price for the order
+	 * @return array
+	 */
+	protected function getTotalsCost($orderId, $priceSubtotal)
+	{
+		$order = $this->getDoctrine()
+			->getRepository('InodataFloraBundle:Order')
+			->find($orderId);
+		
+		$priceShipping = $order->getShipping();
+		if(!$priceShipping){
+			$priceShipping = 0;
+		}
+		$priceDiscount = $order->getDiscount();
+		if (!$priceDiscount){
+			$priceDiscount = 0;
+		}
+		$priceIVA = ($priceSubtotal+$priceShipping-$priceDiscount)*0.16;
+		$priceTotal = $priceSubtotal+$priceShipping-$priceDiscount+$priceIVA;
+		
+		return array('shipping'=>$priceShipping, 'discount' =>$priceDiscount,
+					 'iva' =>$priceIVA, 'subtotal'=>$priceSubtotal, 'total'=>$priceTotal);
+	}
+	
+	public function editAction($id = null)
+	{
+		if ($this->getRestMethod() == 'POST'){
+			$this->preUpdate($id);
+		}
+		
+		return parent::editAction($id);
+	}
+	
+	protected function preUpdate($id)
+	{
+		$em = $this->getDoctrine()->getEntityManager();
+		$orderProducts = $em->getRepository('InodataFloraBundle:OrderProduct')
+			->findByOrderId($id);
+		
+		if ($orderProducts)
+		{
+			foreach ($orderProducts as $product){
+				$em->remove($product);
+			}
+			
+			$em->flush();
+			$em->clear();
+		}
+	}
+	
+	public function configure()
+	{
+		$adminCode = $this->container->get('request')->get('_sonata_admin');
+		
+		if($adminCode){
+			parent::configure();
+		}
 	}
 }
