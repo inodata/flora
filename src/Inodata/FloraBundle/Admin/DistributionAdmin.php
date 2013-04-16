d93026d77b085a2a511bb7cf2a4944b99bf5c41f
Enrique Garcia <enrique@inodata.com.mx>
2013-04-16 17:08:50 -0500

    Agrega esqueleto para modulo de distribucion.

---
+++ src/Inodata/FloraBundle/Admin/DistributionAdmin.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Inodata\FloraBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+
+use Symfony\Component\Form\AbstractType;
+
+class DistributionAdmin extends Admin
+{
+	/**
+	 * @param Sonata\AdminBundle\Form\FormMapper $formMapper
+	 * 
+	 * @return void
+	 */
+	protected function configureFormFields(FormMapper $formMapper)
+	{
+		$formMapper
+			->add('id')
+			->add('messenger_id')
+		;
+	}
+	
+	/**
+	 * @param Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+	 * 
+	 * @return void
+	 */
+	protected function configureListFields(ListMapper $listMapper)
+	{
+		$listMapper
+			->addIdentifier('id')
+			->addIdentifier('messenger_id')
+			->add('_action', 'actions', array(
+				'actions' => array(
+					'view' => array(),
+					'edit' => array(),
+					'delete' => array(),
+				)
+			));
+	}
+	
+	/**
+	 * @param Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+	 * 
+	 * @return void
+	 */
+	
+	protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+	{
+		$datagridMapper
+			->add('id')
+			->addIdentifier('messenger_id')
+		;		
+	}
+	
+} 
