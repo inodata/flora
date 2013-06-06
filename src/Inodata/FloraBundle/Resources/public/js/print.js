e65deb40a9491dc8a26d39958675dc1bbacb7188
Enrique Garcia <enrique@inodata.com.mx>
2013-06-05 23:00:08 -0500

    Update print.js
    Actualiza el script para seleccionar correctamente el tamanio de papel y hacer una pausa para que se impriman los elementos que se renderean al final por AJAX.

--- src/Inodata/FloraBundle/Resources/public/js/print.js
+++ src/Inodata/FloraBundle/Resources/public/js/print.js
@@ -12,9 +12,9 @@ function installjsPrintSetup() {
 //39 : {PD:39, PN: 'na_fanfold-us',PWG:'na_fanfold-us_11x14.875in',Name: 'US Std Fanfold', W: 11, H: 14.875, M: kPaperSizeInches}
 function definePaperSizes(){
   //note
-  jsPrintSetup.definePaperSize(2, 2, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 8.5, 5.5, jsPrintSetup.kPaperSizeInches);
+  jsPrintSetup.definePaperSize(98, 98, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 8.5, 5.5, jsPrintSetup.kPaperSizeInches);
   //card
-  jsPrintSetup.definePaperSize(99, 99, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 4.7, 5.5, jsPrintSetup.kPaperSizeInches);
+  jsPrintSetup.definePaperSize(99, 99, 'na_letter', 'na_letter_4.7x5.5in', 'US Letter', 4.7, 5.5, jsPrintSetup.kPaperSizeInches);
 }
 
 function setupGlobalOptions(){
@@ -41,7 +41,7 @@ function setupGlobalOptions(){
     jsPrintSetup.setOption('footerStrCenter', '');
     jsPrintSetup.setOption('footerStrRight', '');
 
-    definePaperSizes();
+    //definePaperSizes();
 
     // clears user preferences always silent print value
     // to enable using 'printSilent' option
@@ -60,27 +60,27 @@ function printCard(){
   setupGlobalOptions();
   jsPrintSetup.setGlobalOption('paperWidth', 110);
   jsPrintSetup.setGlobalOption('paperHeight', 140);
-  jsPrintSetup.setPrinter('Epson_xp001');
-  jsPrintSetup.setPrinter('PostScript/default');
-  jsPrintSetup.setPaperSizeData(99);
-  jsPrintSetup.print();
+  //jsPrintSetup.setPrinter('Epson_xp001');
+  jsPrintSetup.setPrinter('EPSONLX300');
+  setTimeout('jsPrintSetup.print()', 3000);
 }
 
 function printNote(){
-  
   setupGlobalOptions();
   jsPrintSetup.setGlobalOption('paperWidth', 216);
   jsPrintSetup.setGlobalOption('paperHeight', 140);
   //alert(jsPrintSetup.getPrintersList());
-  jsPrintSetup.setPrinter('Epson_xp002');
-  jsPrintSetup.setPrinter('PostScript/default');
-  jsPrintSetup.setPaperSizeData(2);
-  jsPrintSetup.print();
+  //jsPrintSetup.setPrinter('Epson_xp002');
+  jsPrintSetup.setPrinter('EPSONLX300');
+  //jsPrintSetup.setPrinter('PostScript/default');
+  //this fuction was useless with paper size
+  //jsPrintSetup.setPaperSizeData(2);
+  //add a delay to render correctly all elements fetched via AJAX
+  setTimeout('jsPrintSetup.print()', 3000);
 }
 
 function printDistributionList(){
 	setupGlobalOptions();
-	jsPrintSetup.setPrinter('PostScript/default');
+	setTimeout('jsPrintSetup.print()', 3000);
 	jsPrintSetup.print();
-	//window.close();
 }
