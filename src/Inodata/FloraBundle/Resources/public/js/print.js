4492ab6fba771b17cf03e7c66b6c2a7d6a3bffb2
Heriberto <heri185403@gmail.com>
2013-06-14 14:29:34 -0500

    actualiza los cambios de la impresora

--- src/Inodata/FloraBundle/Resources/public/js/print.js
+++ src/Inodata/FloraBundle/Resources/public/js/print.js
@@ -8,6 +8,16 @@ function installjsPrintSetup() {
     }
 }
 
+//This function was useless handling medium letter size.
+//Define paper size 
+//39 : {PD:39, PN: 'na_fanfold-us',PWG:'na_fanfold-us_11x14.875in',Name: 'US Std Fanfold', W: 11, H: 14.875, M: kPaperSizeInches}
+function definePaperSizes(){
+  //note
+  jsPrintSetup.definePaperSize(50, 50, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 8.5, 5.5, jsPrintSetup.kPaperSizeInches);
+  //card
+  jsPrintSetup.definePaperSize(51, 51, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 4.7, 5.5, jsPrintSetup.kPaperSizeInches);
+}
+
 function setupGlobalOptions(){
   //check if jsPrintSetup is installed
   if (typeof(jsPrintSetup) == 'undefined') {
@@ -32,7 +42,7 @@ function setupGlobalOptions(){
     jsPrintSetup.setOption('footerStrCenter', '');
     jsPrintSetup.setOption('footerStrRight', '');
 
-    definePaperSizes();
+    //definePaperSizes();
 
     // clears user preferences always silent print value
     // to enable using 'printSilent' option
@@ -43,16 +53,6 @@ function setupGlobalOptions(){
   }
 }
 
-//This function was useless handling medium letter size.
-//Define paper size 
-//39 : {PD:39, PN: 'na_fanfold-us',PWG:'na_fanfold-us_11x14.875in',Name: 'US Std Fanfold', W: 11, H: 14.875, M: kPaperSizeInches}
-function definePaperSizes(){
-  //note
-  jsPrintSetup.definePaperSize(50, 50, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 8.5, 5.5, jsPrintSetup.kPaperSizeInches);
-  //card
-  jsPrintSetup.definePaperSize(51, 51, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 4.7, 5.5, jsPrintSetup.kPaperSizeInches);
-}
-
 // Do Print 
 // When print is submitted it is executed asynchronous and
 // script flow continues after print independently of completetion of print process! 
@@ -61,16 +61,22 @@ var print_delay = 3000;
 
 function printCard(){
   setupGlobalOptions();
-  jsPrintSetup.setPaperSizeData(51);
-  jsPrintSetup.setPrinter('PostScript/default');
+  jsPrintSetup.definePaperSize(1, 1, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 4.7, 5.5, jsPrintSetup.kPaperSizeInches);
+  jsPrintSetup.setPaperSizeData(1);
+  jsPrintSetup.setPrinter('\\\\flora\\Tarjetas');
   //alert(jsPrintSetup.getPaperMeasure());
   setTimeout('jsPrintSetup.print()', print_delay);
 }
 
 function printNote(){
   setupGlobalOptions();
-  jsPrintSetup.setPaperSizeData(50);
-  jsPrintSetup.setPrinter('PostScript/default');
+  //jsPrintSetup.setPaperSizeData(1);
+  //jsPrintSetup.setOption ("paperWidth", 216);
+  //jsPrintSetup.setOption ("paperHeight", 140);
+  jsPrintSetup.definePaperSize(100, 100, 'na_letter', 'na_letter_8.5x11in', 'Notas', 8.5, 5.5, jsPrintSetup.kPaperSizeInches);
+  jsPrintSetup.setPaperSizeData(100);
+  //alert(jsPrintSetup.getPaperSizeList());
+  jsPrintSetup.setPrinter('\\\\flora\\Notas');
   //alert(jsPrintSetup.getPaperMeasure());
   //add a delay to render correctly all elements fetched via AJAX
   setTimeout('jsPrintSetup.print()', print_delay);
@@ -78,8 +84,9 @@ function printNote(){
 
 function printInvoice(){
   setupGlobalOptions();
+  jsPrintSetup.definePaperSize(1, 1, 'na_letter', 'na_letter_8.5x11in', 'US Letter', 8.5, 11, jsPrintSetup.kPaperSizeInches);
   jsPrintSetup.setPaperSizeData(1);
-  jsPrintSetup.setPrinter('PostScript/default');
+  jsPrintSetup.setPrinter('Facturas');
   //alert(jsPrintSetup.getPaperMeasure());
   //add a delay to render correctly all elements fetched via AJAX
   setTimeout('jsPrintSetup.print()', print_delay);
@@ -87,6 +94,6 @@ function printInvoice(){
 
 //FIXME: Revisar la impresion de esta lista y seleccionar impresora.
 function printDistributionList(){
-	setupGlobalOptions();
-	setTimeout('jsPrintSetup.print()', 3000);
+  setupGlobalOptions();
+  setTimeout('jsPrintSetup.print()', 3000);
 }
