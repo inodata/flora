0316e5c814b7b7cfa3b6d19bf7ab3804a3a31211
Heriberto Monterrubio <heriberto.monterrubio@komodot.com.mx>
2015-12-16 17:19:24 -0600

    corrige admin de distribucion

--- src/Inodata/FloraBundle/Admin/DistributionAdmin.php
+++ src/Inodata/FloraBundle/Admin/DistributionAdmin.php
@@ -55,8 +55,8 @@ class DistributionAdmin extends Admin
 			->add('messenger', null, array(
 					'label' => 'label.messenger'
 				))
-			->addIdentifier('id', null, array(
-					'label' => 'label.distribution_id',
+			->addIdentifier('id', 'entity', array(
+					'label' => 'label.distribution_id', 'admin_code' => 'admin.order'
 				))
 			->add('firstProduct', null, array(
 					'label' => 'label.distribution_product',
