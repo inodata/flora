c98e1d8153a296a72612e14cc8f590543e716d08
Enrique García <enrique@inodata.com.mx>
2016-12-21 12:04:38 -0600

    Arregla error de sintaxis

--- src/Inodata/FloraBundle/Form/Type/CustomerType.php
+++ src/Inodata/FloraBundle/Form/Type/CustomerType.php
@@ -3,25 +3,26 @@
 namespace Inodata\FloraBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class CustomerType extends AbstractType
 {
-	public function buildForm(FormBuilderInterface $builder, array $options)
-	{
-		$builder
-			->add('member', 'genemu_jqueryautocompleter_entity', array(
-            'class' => 'Genemu\Bundle\EntityBundle\Entity\Member',
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'member',
+            'genemu_jqueryautocompleter_entity',
+            ['class' => 'Genemu\Bundle\EntityBundle\Entity\Member',]
         );
-	}
+    }
 
-	public function getParent()
-	{
-		return 'text';
-	}
+    public function getParent()
+    {
+        return 'text';
+    }
 
-	public function getName()
-	{
-		return 'inodata_customer_type';
-	}
+    public function getName()
+    {
+        return 'inodata_customer_type';
+    }
 }
