9e6ff074a1f1ed2a4b738bfe8a81607f6a9773d2
Enrique García <enrique@inodata.com.mx>
2017-04-30 20:41:29 -0500

    Actualiza archivos de symfony

--- web/app.php
+++ web/app.php
@@ -1,14 +1,17 @@
 <?php
-umask(0002); //TODO: Remover esto
+// umask(0002); //TODO: Remover esto
 
 use Symfony\Component\HttpFoundation\Request;
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
+
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
+
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
