7e381305fb42cf7de6acacdf3a5a1bc968347ee6
Enrique García <enrique@inodata.com.mx>
2021-01-27 21:54:17 -0600

    Deshabilita la compia de impresión para el archivo

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -3,3 +3,4 @@ app/console assetic:dump --env=prod
 app/console assets:install --env=prod --symlink
 app/console cache:clear --env=prod
 chmod -R 777 app/cache/ app/logs/
+chown -R inodata:inodata *
