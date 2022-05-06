4066aa43ebc42a1a8ca8d4a3a077b39651f71a68
Enrique García <enrique@inodata.com.mx>
2022-05-06 11:41:05 -0500

    Actualización de precio de envío a 125

--- deploy.prod.sh
+++ deploy.prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-app/console assetic:dump --env=prod
-app/console assets:install --env=prod --symlink
-app/console cache:clear --env=prod
+php72 app/console assetic:dump --env=prod
+php72 app/console assets:install --env=prod --symlink
+php72 app/console cache:clear --env=prod
 chmod -R 777 app/cache/ app/logs/
 chown -R inodata:inodata *
